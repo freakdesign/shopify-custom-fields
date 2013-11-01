a868b381742203f91881ff5c8ed5d679b85f7403
Jason Bowman <jason@freakdesign.com.au>
2013-11-01 11:54:52 +1000

    Updates to reflect github paths

--- freakdesign_custom_fields.load.js
+++ freakdesign_custom_fields.load.js
@@ -26,4 +26,7 @@ shopify.freakdesign.com.au
 Don't be a tool and pass this off as your own. Contribute!
 
 */
-var custom_js=document.createElement("script");custom_js.type="text/javascript";custom_js.src="//dl.dropboxusercontent.com/s/ub4haw5lc4urzvm/freakdesign_custom_fields.js";document.body.appendChild(custom_js);
+var custom_js=document.createElement("script");
+custom_js.type="text/javascript";
+custom_js.src="//rawgithub.com/freakdesign/shopify-custom-fields/master/freakdesign_custom_fields.min.js";
+document.body.appendChild(custom_js);
