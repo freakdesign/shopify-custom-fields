09cc796546b92490364e4972ecede221340a43af
Jason Bowman <jason@freakdesign.com.au>
2013-11-01 11:47:34 +1000

    Initial commit

---
+++ freakdesign_custom_fields.load.js
@@ -0,0 +1,29 @@
+/*
+
+
+      ############# 
+    ##            *## 
+   #        %      **#
+  #        %%%    ***#
+ #       %%F%D%%   ****#
+#          %%%    *****#
+#   ###     %     ###***# 
+#  # ####       #### #**# 
+#  #     #     #     #**# 
+#   #####  # #  #####***# 
+#         #   #  *******# 
+ ### #           **# ### 
+     # - - - - - - #                 
+      | | | | | | |
+	
+     freakdesign.com               
+
+CUSTOM FIELDS
+
+Written by jason from freakdesign
+freakdesign.com.au
+shopify.freakdesign.com.au
+Don't be a tool and pass this off as your own. Contribute!
+
+*/
+var custom_js=document.createElement("script");custom_js.type="text/javascript";custom_js.src="//dl.dropboxusercontent.com/s/ub4haw5lc4urzvm/freakdesign_custom_fields.js";document.body.appendChild(custom_js);
