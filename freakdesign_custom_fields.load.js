857b3bb3c6962068cd1b4c71096b65d846dd33a7
Jason Bowman <jason@freakdesign.com.au>
2014-08-13 04:20:43 +1000

    Move script locations

--- freakdesign_custom_fields.load.js
+++ freakdesign_custom_fields.load.js
@@ -29,4 +29,4 @@ Don't be a tool and pass this off as your own. Contribute!
 var custom_js=document.createElement("script");
 custom_js.type="text/javascript";
 custom_js.src="//rawgithub.com/freakdesign/shopify-custom-fields/master/freakdesign_custom_fields.min.js";
-document.body.appendChild(custom_js);
+document.getElementsByTagName('head')[0].appendChild(custom_js)
